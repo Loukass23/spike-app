4ea36e04f35c018d420f1ab8fd80a6967ab7649e
Lucas D <lucas@codeacademyberlin.com>
2024-12-16 21:41:55 +0100

    moo

--- app/characters/[id]/page.tsx
+++ app/characters/[id]/page.tsx
@@ -1,23 +1,47 @@
 import { Character } from '@/types/rickandmorty'
 import Image from 'next/image'
+import { notFound } from 'next/navigation'
 import React from 'react'
 
-type Props = {
-  params: { id: string }
-  searchParams: { [key: string]: string | string[] | undefined }
+async function getCharacter(id: string) {
+  const res = await fetch(`https://rickandmortyapi.com/api/character/${id}`, {
+    cache: 'force-cache',
+  })
+  const character: Character = await res.json()
+  if (!character) notFound()
+  return character
 }
 
-const CharacterDetails = async ({ params, searchParams }: Props) => {
-  console.log({ params, searchParams });
-  const response = await fetch(`https://rickandmortyapi.com/api/character/${params.id}`);
-  const result = await response.json() as Character;
-  if (response.ok) return (
+export async function generateStaticParams() {
+  const { results: characters } = await fetch('https://rickandmortyapi.com/api/character').then((res) => res.json())
+  return characters.map((character: Character) => ({
+    id: String(character.id),
+  }))
+}
+
+export async function generateMetadata({
+  params,
+}: {
+  params: Promise<{ id: string }>
+}) {
+  const { id } = await params
+  const character = await getCharacter(id)
+
+  return {
+    name: character.name,
+  }
+}
+
+// Multiple versions of this page will be statically generated
+// using the `params` returned by `generateStaticParams`
+export default async function Page({ params }: { params: { id: string } }) {
+  const { id } = await params
+  const response = await fetch(`https://rickandmortyapi.com/api/character/${id}`)
+  const character = await response.json() as Character
+  return (
     <div>
-      <h1>{result.name}</h1>
-      <Image src={result.image} alt={result.name} width={300} height={300} />
+      <h1>{character.name}</h1>
+      <Image src={character.image} alt={character.name} width={300} height={300} />
     </div>
   )
-  return <p>Something went wrong....</p>
 }
-
-export default CharacterDetails
--- app/streaming/page.tsx
+++ app/streaming/page.tsx
@@ -1,13 +1,12 @@
-import { FetchResult } from "@/types/rickandmorty";
-import { Suspense } from "react";
+// import { Suspense } from "react";
 import CharactersStreaming from "./characters-streaming";
 
 export const getCharactersPromise = () => {
-  const promise: Promise<FetchResult> = fetch("https://rickandmortyapi.com/api/character").then(response => response.json());
+  const promise = fetch("https://rickandmortyapi.com/api/character").then(response => response.json());
   return promise;
 }
 
-export default async function StreamingPage() {
+export default function StreamingPage() {
 
   // we do not await for the promise to resolve as we are only using the data for the UI
   const charactersPromise = getCharactersPromise()
@@ -16,13 +15,13 @@ export default async function StreamingPage() {
     <div>
       <h4>Using Suspense Component </h4>
       {/* Any content wrapped in a <Suspense> boundary will be streamed */}
-      <Suspense fallback={<p>Suspense Loading...</p>}>
+      {/* <Suspense fallback={<p>Suspense Loading...</p>}>
         <CharactersStreaming charactersPromise={charactersPromise} />
-      </Suspense>
+      </Suspense> */}
 
 
-      {/* <h4>Using loading file </h4>
-      <CharactersStreaming charactersPromise={charactersPromise} /> */}
+      <h4>Using loading file </h4>
+      <CharactersStreaming charactersPromise={charactersPromise} />
     </div>
   );
 }
