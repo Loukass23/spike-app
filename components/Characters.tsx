f1b3c233fc515dd9efd18b1590dac710defda2de
Lucas Dupias <lucas@codeacademyberlin.com>
2025-01-14 14:23:55 +0100

    add img

--- components/Characters.tsx
+++ components/Characters.tsx
@@ -1,6 +1,7 @@
 import { Character } from '@/types';
 import Link from 'next/link';
 import React from 'react'
+import Image from 'next/image'
 
 
 async function Characters({ characters }: { characters: Character[] }) {
@@ -10,6 +11,7 @@ async function Characters({ characters }: { characters: Character[] }) {
       <ul>
         {characters.map((character) => {
           return <li key={character.id}>
+            <Image src={character.image} alt={character.name} width={300} height={300} />
             <Link href={`/characters/${character.id}`}>{character.name}</Link>
           </li>
         })}
