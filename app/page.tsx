0b77e5439d5f480405ced3a93fbf4024f085ffda
Lucas Dupias <lucas@codeacademyberlin.com>
2025-01-14 11:49:02 +0100

    edit title

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="grid items-center justify-items-center min-h-screen p-8 pb-20 sm:p-20 font-[family-name:var(--font-geist-sans)]">
 
-      <h1 className="text-3xl">Next.js</h1>
+      <h1 className="text-3xl">Next.js and Git</h1>
 
       <Link href="/characters">Static Site Generation</Link>
       <Link href="/client">Client Side Rendering</Link>
